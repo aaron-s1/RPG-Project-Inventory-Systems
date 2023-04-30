76a0f899e58b3456ec5d807715347d6cd158f361
aaron <aaron_swan@outlook.com>
2023-04-29 17:14:42 -0700

    Show & Hide Panels

---
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventoryExample.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [SerializeField] KeyCode menuToggleKey = KeyCode.Escape;
+        [SerializeField] KeyCode iconToggleKey = KeyCode.Escape;
+
+        [SerializeField] GameObject menuContainer = null;
+        [SerializeField] GameObject iconContainer = null;
+
+        void Start() 
+        {
+            menuContainer.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(menuToggleKey))
+                menuContainer.SetActive(!menuContainer.activeSelf);
+
+            if (Input.GetKeyDown(iconToggleKey))
+                iconContainer.SetActive(!iconContainer.activeSelf);
+        }
+    }
+}
